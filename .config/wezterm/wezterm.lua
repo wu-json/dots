74d1ba8c9060206c029760bec338684fd29cf111
wu-json <jason.wu2153@gmail.com>
2023-12-07 14:04:46 -0500

    feat(wezterm): add close pane keymap

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,12 @@
 local wezterm = require("wezterm")
+local keymaps = require("keymaps")
 local visuals = require("visuals")
+
 local config = {}
 
 config = wezterm.config_builder()
 
+keymaps.apply_to_config(config)
 visuals.apply_to_config(config)
 
 return config
