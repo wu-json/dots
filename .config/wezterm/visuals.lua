0ef1af80b4f335c3b926f4a45b3587b552d59fa9
wu-json <jason.wu2153@gmail.com>
2023-12-08 22:21:27 -0500

    feat: hide tab bar if single tab

--- .config/wezterm/visuals.lua
+++ .config/wezterm/visuals.lua
@@ -3,6 +3,7 @@ local module = {}
 function module.apply_to_config(config)
 	config.font_size = 16.0
 	config.color_scheme = "Tokyo Night"
+	config.hide_tab_bar_if_only_one_tab = true
 end
 
 return module
