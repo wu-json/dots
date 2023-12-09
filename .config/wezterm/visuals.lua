919a7018b4509d27da459345fb14788e7081f92b
wu-json <jason.wu2153@gmail.com>
2023-12-08 22:48:13 -0500

    feat: themes

--- .config/wezterm/visuals.lua
+++ .config/wezterm/visuals.lua
@@ -2,7 +2,7 @@ local module = {}
 
 function module.apply_to_config(config)
 	config.font_size = 16.0
-	config.color_scheme = "Tokyo Night"
+	config.color_scheme = "Panda (Gogh)"
 	config.hide_tab_bar_if_only_one_tab = true
 end
 
