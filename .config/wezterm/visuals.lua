07ac499ff1c6acbfe26a9b924e99c652b6d865a8
wu-json <jason.wu2153@gmail.com>
2023-12-23 13:57:55 -0500

    feat: back to panda

--- .config/wezterm/visuals.lua
+++ .config/wezterm/visuals.lua
@@ -2,7 +2,7 @@ local module = {}
 
 function module.apply_to_config(config)
 	config.font_size = 16.0
-	config.color_scheme = "Nebula (base16)"
+	config.color_scheme = "Panda (Gogh)"
 	config.hide_tab_bar_if_only_one_tab = true
 	config.window_background_opacity = 0.9
 end
