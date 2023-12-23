e92e8ab363433d7762f4d03bd5b13dbb38395836
wu-json <jason.wu2153@gmail.com>
2023-12-23 13:51:26 -0500

    feat: increase bg opacity

--- .config/wezterm/visuals.lua
+++ .config/wezterm/visuals.lua
@@ -4,7 +4,7 @@ function module.apply_to_config(config)
 	config.font_size = 16.0
 	config.color_scheme = "Nebula (base16)"
 	config.hide_tab_bar_if_only_one_tab = true
-	config.window_background_opacity = 0.95
+	config.window_background_opacity = 0.97
 end
 
 return module
