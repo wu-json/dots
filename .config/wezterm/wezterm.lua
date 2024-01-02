23a2a700eb2c296a2195e86ddfd7211975be85db
wu-json <jason.wu2153@gmail.com>
2024-01-01 19:30:36 -0500

    feat: fish shell

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require("wezterm")
 local keymaps = require("keymaps")
+local shell = require("shell")
 local visuals = require("visuals")
 
 local config = {}
@@ -7,6 +8,7 @@ local config = {}
 config = wezterm.config_builder()
 
 keymaps.apply_to_config(config)
+shell.apply_to_config(config)
 visuals.apply_to_config(config)
 
 return config
