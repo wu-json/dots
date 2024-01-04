96723069216b0dd993d15136ff478863b2447669
wu-json <jason.wu2153@gmail.com>
2024-01-03 20:50:52 -0500

    feat: remove bash aliases

--- .aliases.sh
+++
@@ -1,7 +0,0 @@
-# GitHub Branch Clean
-# Checks out main, deletes all other branches, and pulls main.
-alias x-ghbc='(
-    git checkout main
-    git branch | grep -v "main" | xargs git branch -D
-    git pull
-)'
