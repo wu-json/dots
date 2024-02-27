4739132ab48377fb731bcb9d1e6d96e742755357
wu-json <jason.wu2153@gmail.com>
2024-02-27 17:13:17 -0500

    feat: mkdwon preview

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -1,10 +1,3 @@
 return {
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-  },
+  { "iamcco/markdown-preview.nvim" },
 }
