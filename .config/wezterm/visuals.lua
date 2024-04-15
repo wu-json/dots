eb6edf1df38e8abd1a2def118595cdc050df3adb
wu-json <jason.wu2153@gmail.com>
2024-04-15 15:15:31 -0400

    feat: update wezterm themes

--- .config/wezterm/visuals.lua
+++ .config/wezterm/visuals.lua
@@ -1,7 +1,7 @@
 local module = {}
 
 function module.apply_to_config(config)
-	config.color_scheme = "Panda (Gogh)"
+	config.color_scheme = "rose-pine"
 	config.font_size = 16.0
 	config.hide_tab_bar_if_only_one_tab = true
 	config.macos_window_background_blur = 10
