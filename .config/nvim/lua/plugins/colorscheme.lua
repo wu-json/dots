499e4f565cc01d6fe3e67bf70567f555aa65a084
wu-json <jason.wu2153@gmail.com>
2024-04-15 15:01:32 -0400

    feat: cleanup

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,5 @@
 return {
   {
-    "EdenEast/nightfox.nvim",
-    opts = {
-      options = {
-        transparent = true,
-      },
-      groups = {
-        all = {
-          NormalFloat = { fg = "fg1", bg = "NONE" },
-        },
-      },
-    },
-  },
-  {
     "rose-pine/neovim",
     name = "rose-pine",
     opts = {
