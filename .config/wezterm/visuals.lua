ad9a8de1775d61caa30d9c51ca6720887b760d52
wu-json <jason.wu2153@gmail.com>
2024-04-15 15:01:06 -0400

    feat: cleanup

--- .config/wezterm/visuals.lua
+++ .config/wezterm/visuals.lua
@@ -1,11 +1,12 @@
 local module = {}
 
 function module.apply_to_config(config)
-	config.font_size = 16.0
 	config.color_scheme = "Panda (Gogh)"
+	config.font_size = 16.0
 	config.hide_tab_bar_if_only_one_tab = true
+	config.macos_window_background_blur = 10
 	config.window_background_opacity = 0.92
-	config.macos_window_background_blur = 15
+	config.window_decorations = "RESIZE"
 end
 
 return module
