3f0ccfbe28d9a0b55e7c10603c42cf13d13e5d0c
wu-json <jason.wu2153@gmail.com>
2024-06-06 18:44:15 -0700

    feat: more consolidation

--- .config/nvim/lua/plugins/lang.lua
+++ .config/nvim/lua/plugins/lang.lua
@@ -28,4 +28,22 @@ return {
       })
     end,
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "go",
+        "gomod",
+        "gowork",
+        "gosum",
+        "hcl",
+        "python",
+        "rst",
+        "svelte",
+        "terraform",
+        "toml",
+        "yaml",
+      },
+    },
+  },
 }
