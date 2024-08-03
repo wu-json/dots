5068c7696eedc98f20e3350ff351660306207947
Jason Wu <45532884+wu-json@users.noreply.github.com>
2024-08-03 15:58:31 -0400

    feat(theme): kanagawa (#1)
    * feat: wezterm theme
    
    * feat: get term colors working
    
    * feat: aesthetics
    
    * feat: remove gutters
    
    * feat: kasugano
    
    * feat: kanagawa dragon
    
    * fix: borders
    
    * fix: theme
    
    * feat: use dragon
    
    * docs: readme
    
    * feat: adjust visuals
    
    * feat: adjust visuals
    
    * docs: readme

--- .config/nvim/lua/plugins/aesthetics.lua
+++ .config/nvim/lua/plugins/aesthetics.lua
@@ -1,22 +1,40 @@
 return {
   {
     "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "nordfox",
-    },
+    opts = { colorscheme = "kanagawa-dragon" },
   },
   {
-    "EdenEast/nightfox.nvim",
-    opts = {
-      options = {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    config = function()
+      require("kanagawa").setup({
+        compile = true,
         transparent = true,
-      },
-      groups = {
-        all = {
-          NormalFloat = { fg = "fg1", bg = "NONE" },
+        colors = {
+          theme = {
+            all = {
+              ui = { bg_gutter = "none", float = { bg = "none" } },
+            },
+          },
         },
-      },
-    },
+        overrides = function(colors)
+          local theme = colors.theme
+          return {
+            NormalFloat = { bg = "none" },
+            FloatBorder = { bg = "none" },
+            FloatTitle = { bg = "none" },
+            TelescopeTitle = { fg = theme.ui.special, bg = "none", bold = true },
+            TelescopeBorder = { bg = "none" },
+            TelescopePromptNormal = { bg = "none" },
+            TelescopePromptBorder = { fg = "none", bg = "none" },
+            TelescopeResultsNormal = { fg = "none", bg = "none" },
+            TelescopeResultsBorder = { fg = "none", bg = "none" },
+            TelescopePreviewNormal = { bg = "none" },
+            TelescopePreviewBorder = { bg = "none", fg = "none" },
+          }
+        end,
+      })
+    end,
   },
   {
     "rcarriga/nvim-notify",
