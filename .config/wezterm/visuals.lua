3b37cafb1bef32e91a257dce4347f53f5d349780
Jason Wu <45532884+wu-json@users.noreply.github.com>
2024-08-03 15:58:31 -0400

    feat(theme): kanagawa (#1)
    * feat: wezterm theme
    
    * feat: get term colors working
    
    * feat: aesthetics
    
    * feat: remove gutters
    
    * feat: kasugano
    
    * feat: kanagawa dragon
    
    * fix: borders
    
    * fix: theme
    
    * feat: use dragon
    
    * docs: readme
    
    * feat: adjust visuals
    
    * feat: adjust visuals
    
    * docs: readme

--- .config/wezterm/visuals.lua
+++ .config/wezterm/visuals.lua
@@ -1,11 +1,11 @@
 local module = {}
 
 function module.apply_to_config(config)
-	config.color_scheme = "nightfox"
+	config.color_scheme = "Kanagawa Dragon (Gogh)"
 	config.font_size = 16.0
 	config.hide_tab_bar_if_only_one_tab = true
-	config.macos_window_background_blur = 2
-	config.window_background_opacity = 0.92
+	config.macos_window_background_blur = 10
+	config.window_background_opacity = 0.82
 	config.window_decorations = "RESIZE"
 end
 
