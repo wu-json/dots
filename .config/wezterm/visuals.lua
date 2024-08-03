17d1c6e68bc56040ab60525cf8788c3010f7c8d4
wu-json <jason.wu2153@gmail.com>
2024-08-03 13:41:35 -0400

    feat: nordfox

--- .config/wezterm/visuals.lua
+++ .config/wezterm/visuals.lua
@@ -1,10 +1,10 @@
 local module = {}
 
 function module.apply_to_config(config)
-	config.color_scheme = "rose-pine"
+	config.color_scheme = "nightfox"
 	config.font_size = 16.0
 	config.hide_tab_bar_if_only_one_tab = true
-	config.macos_window_background_blur = 10
+	config.macos_window_background_blur = 2
 	config.window_background_opacity = 0.92
 	config.window_decorations = "RESIZE"
 end
