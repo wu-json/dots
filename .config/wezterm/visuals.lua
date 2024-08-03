08b9a3142d4168952338d79ba763adcb20a701d2
wu-json <jason.wu2153@gmail.com>
2024-08-03 15:54:20 -0400

    feat: adjust visuals

--- .config/wezterm/visuals.lua
+++ .config/wezterm/visuals.lua
@@ -4,8 +4,8 @@ function module.apply_to_config(config)
 	config.color_scheme = "Kanagawa Dragon (Gogh)"
 	config.font_size = 16.0
 	config.hide_tab_bar_if_only_one_tab = true
-	config.macos_window_background_blur = 2
-	config.window_background_opacity = 0.90
+	config.macos_window_background_blur = 8
+	config.window_background_opacity = 0.82
 	config.window_decorations = "RESIZE"
 end
 
