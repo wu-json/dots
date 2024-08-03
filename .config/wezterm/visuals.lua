a0315e50209877bb49daab020f454034e857f3bc
wu-json <jason.wu2153@gmail.com>
2024-08-03 16:03:43 -0400

    feat: darken bg

--- .config/wezterm/visuals.lua
+++ .config/wezterm/visuals.lua
@@ -5,7 +5,7 @@ function module.apply_to_config(config)
 	config.font_size = 16.0
 	config.hide_tab_bar_if_only_one_tab = true
 	config.macos_window_background_blur = 10
-	config.window_background_opacity = 0.82
+	config.window_background_opacity = 0.88
 	config.window_decorations = "RESIZE"
 end
 
