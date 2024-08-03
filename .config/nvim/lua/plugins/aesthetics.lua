de41eb066fc5f666cca5ad32e9a793b8017b51a8
wu-json <jason.wu2153@gmail.com>
2024-08-03 15:28:46 -0400

    fix: borders

--- .config/nvim/lua/plugins/aesthetics.lua
+++ .config/nvim/lua/plugins/aesthetics.lua
@@ -7,21 +7,35 @@ return {
   },
   {
     "rebelot/kanagawa.nvim",
-    opts = {
-      transparent = true,
-      colors = {
-        theme = {
-          all = {
-            ui = {
-              bg_gutter = "none",
-              float = {
-                bg = "none",
-              },
+    lazy = false,
+    config = function()
+      require("kanagawa").setup({
+        compile = true,
+        transparent = true,
+        colors = {
+          theme = {
+            all = {
+              ui = { bg_gutter = "none", float = { bg = "none" } },
             },
           },
         },
-      },
-    },
+        overrides = function(colors)
+          local theme = colors.theme
+          return {
+            NormalFloat = { bg = "none" },
+            FloatBorder = { bg = "none" },
+            FloatTitle = { bg = "none" },
+            TelescopeTitle = { fg = theme.ui.special, bold = true },
+            TelescopePromptNormal = { bg = "none" },
+            TelescopePromptBorder = { fg = "none", bg = "none" },
+            TelescopeResultsNormal = { fg = "none", bg = "none" },
+            TelescopeResultsBorder = { fg = "none", bg = "none" },
+            TelescopePreviewNormal = { bg = "none" },
+            TelescopePreviewBorder = { bg = "none", fg = "none" },
+          }
+        end,
+      })
+    end,
   },
   {
     "rcarriga/nvim-notify",
