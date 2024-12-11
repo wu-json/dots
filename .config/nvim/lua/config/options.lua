962e76fad422674243ab4c7fa78b2792ec94c54a
wu-json <jason.wu2153@gmail.com>
2024-12-10 19:35:39 -0800

    feat: prettier config

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Avoid prettier conflicts with biome: https://www.lazyvim.org/extras/formatting/biome
+vim.g.lazyvim_prettier_needs_config = true
