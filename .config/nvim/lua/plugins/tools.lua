85aec08ccdae75d605e2893403bfc63987d53009
wu-json <jason.wu2153@gmail.com>
2024-12-13 17:24:07 -0800

    feat: mini surround

--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -1,12 +1,9 @@
 return {
   {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
+    "echasnovski/mini.nvim",
+    version = "*",
     config = function()
-      require("nvim-surround").setup({
-        -- Configuration here, or leave empty to use defaults
-      })
+      require("mini.surround").setup()
     end,
   },
   { "iamcco/markdown-preview.nvim" },
