434d933c9a75f72f3518216525113f0150785598
wu-json <jason.wu2153@gmail.com>
2024-12-13 16:40:03 -0800

    feat: disable wezterm tab bar

--- .config/wezterm/visuals.lua
+++ .config/wezterm/visuals.lua
@@ -4,6 +4,7 @@ function module.apply_to_config(config)
 	config.color_scheme = "rose-pine"
 	config.font_size = 16.0
 	config.hide_tab_bar_if_only_one_tab = true
+	config.enable_tab_bar = false
 	config.macos_window_background_blur = 10
 	config.window_background_opacity = 0.95
 	config.window_decorations = "RESIZE"
