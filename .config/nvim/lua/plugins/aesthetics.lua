1253984a1406b43fa8c9afff55c2be412ac432f3
wu-json <jason.wu2153@gmail.com>
2024-12-13 16:07:31 -0800

    feat: back to rose pine

--- .config/nvim/lua/plugins/aesthetics.lua
+++ .config/nvim/lua/plugins/aesthetics.lua
@@ -1,40 +1,14 @@
 return {
   {
     "LazyVim/LazyVim",
-    opts = { colorscheme = "kanagawa-dragon" },
+    opts = { colorscheme = "rose-pine" },
   },
   {
-    "rebelot/kanagawa.nvim",
-    lazy = false,
-    config = function()
-      require("kanagawa").setup({
-        compile = true,
-        transparent = true,
-        colors = {
-          theme = {
-            all = {
-              ui = { bg_gutter = "none", float = { bg = "none" } },
-            },
-          },
-        },
-        overrides = function(colors)
-          local theme = colors.theme
-          return {
-            NormalFloat = { bg = "none" },
-            FloatBorder = { bg = "none" },
-            FloatTitle = { bg = "none" },
-            TelescopeTitle = { fg = theme.ui.special, bg = "none", bold = true },
-            TelescopeBorder = { bg = "none" },
-            TelescopePromptNormal = { bg = "none" },
-            TelescopePromptBorder = { fg = "none", bg = "none" },
-            TelescopeResultsNormal = { fg = "none", bg = "none" },
-            TelescopeResultsBorder = { fg = "none", bg = "none" },
-            TelescopePreviewNormal = { bg = "none" },
-            TelescopePreviewBorder = { bg = "none", fg = "none" },
-          }
-        end,
-      })
-    end,
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      styles = { transparency = true },
+    },
   },
   {
     "rcarriga/nvim-notify",
