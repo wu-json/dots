dbbc66a6e9a63c5f67930b2967a00cccc8935d89
wu-json <jason.wu2153@gmail.com>
2024-12-15 00:31:33 -0800

    feat: nightfox

--- .config/nvim/lua/plugins/aesthetics.lua
+++ .config/nvim/lua/plugins/aesthetics.lua
@@ -1,14 +1,20 @@
 return {
   {
-    "LazyVim/LazyVim",
-    opts = { colorscheme = "rose-pine" },
+    "EdenEast/nightfox.nvim",
+    opts = {
+      options = {
+        transparent = true,
+      },
+      groups = {
+        all = {
+          NormalFloat = { fg = "fg1", bg = "NONE" },
+        },
+      },
+    },
   },
   {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    opts = {
-      styles = { transparency = true },
-    },
+    "LazyVim/LazyVim",
+    opts = { colorscheme = "nightfox" },
   },
   {
     "rcarriga/nvim-notify",
