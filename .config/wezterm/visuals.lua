3696fb413da460c4b77fed370d52f20fa07e187b
wu-json <jason.wu2153@gmail.com>
2024-12-15 00:29:03 -0800

    feat: nightfox

--- .config/wezterm/visuals.lua
+++ .config/wezterm/visuals.lua
@@ -1,7 +1,7 @@
 local module = {}
 
 function module.apply_to_config(config)
-	config.color_scheme = "rose-pine"
+	config.color_scheme = "nightfox"
 	config.font_size = 16.0
 	config.hide_tab_bar_if_only_one_tab = true
 	config.enable_tab_bar = false
