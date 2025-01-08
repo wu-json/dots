e709dfbc4d377757ab7847c9c59a4ba2ef50dd9c
wu-json <jason.wu2153@gmail.com>
2025-01-08 12:38:49 -0500

    feat: bump max fps

--- .config/wezterm/visuals.lua
+++ .config/wezterm/visuals.lua
@@ -2,10 +2,11 @@ local module = {}
 
 function module.apply_to_config(config)
 	config.color_scheme = "nightfox"
+	config.enable_tab_bar = false
 	config.font_size = 16.0
 	config.hide_tab_bar_if_only_one_tab = true
-	config.enable_tab_bar = false
 	config.macos_window_background_blur = 10
+	config.max_fps = 120
 	config.window_background_opacity = 0.95
 	config.window_decorations = "RESIZE"
 end
