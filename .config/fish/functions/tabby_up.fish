b6ee003ecce58738e2e1f7975d736777c7233441
wu-json <jason.wu2153@gmail.com>
2025-01-20 07:42:28 -0800

    feat: remove tabby

--- .config/fish/functions/tabby_up.fish
+++
@@ -1,7 +0,0 @@
-function tabby_up
-    tabby serve \
-        --device metal \
-        --model StarCoder-1B \
-        --chat-model Qwen2-1.5B-Instruct \
-        --port 8085
-end
--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -8,13 +8,4 @@ return {
   },
   { "iamcco/markdown-preview.nvim" },
   { "rhysd/git-messenger.vim" },
-  {
-    "TabbyML/vim-tabby",
-    lazy = false,
-    dependencies = { "neovim/nvim-lspconfig" },
-    init = function()
-      vim.g.tabby_agent_start_command = { "npx", "tabby-agent", "--stdio" }
-      vim.g.tabby_inline_completion_trigger = "auto"
-    end,
-  },
 }
