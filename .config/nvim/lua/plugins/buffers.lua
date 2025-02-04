c4d892f08a4d62da519d4d5f5011f69e0cb99e4f
wu-json <jason.wu2153@gmail.com>
2025-02-04 14:04:35 -0500

    fix: remove buffer hack

--- .config/nvim/lua/plugins/buffers.lua
+++ .config/nvim/lua/plugins/buffers.lua
@@ -1,9 +1,3 @@
 return {
-  {
-    -- Fixes buffer line error
-    -- https://www.reddit.com/r/neovim/comments/1ctx4z7/buffer_line_error_after_update/
-    "akinsho/bufferline.nvim",
-    version = "*",
-  },
   { "schickling/vim-bufonly" },
 }
