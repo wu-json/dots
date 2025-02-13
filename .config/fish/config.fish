8e925305763dacfbc7c545e064128e682512f9ea
wu-json <jason.wu2153@gmail.com>
2025-02-13 10:21:06 -0800

    feat: sync and submit graphite

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,10 @@
 # GitHub
 alias ghcl="github_clean"
+alias gtss="graphite_sync_and_submit"
 
 # Eza
 alias ls="eza"
 alias tree="eza --tree"
-
-# Tabby ML
-alias tabby:up="tabby_up"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
---
+++ .config/fish/functions/graphite_sync_and_submit.fish
@@ -0,0 +1,4 @@
+function graphite_sync_and_submit
+    gt sync
+    gt submit
+end
