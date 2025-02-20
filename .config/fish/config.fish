27e183abd8ceca37c4b0b1061dd437fa585c2080
wu-json <jason.wu2153@gmail.com>
2025-02-20 12:10:32 -0800

    feat: cleanup config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,10 +6,9 @@ alias gtss="graphite_sync_and_submit"
 alias ls="eza"
 alias tree="eza --tree"
 
+# OrbStack
+source ~/.orbstack/shell/init.fish 2>/dev/null || :
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
-# Added by OrbStack: command-line tools and integration
-# This won't be added again if you remove it.
-source ~/.orbstack/shell/init.fish 2>/dev/null || :
