5144cf662c59fc9b3173d570f21c3de3a9da54be
wu-json <jason.wu2153@gmail.com>
2025-02-20 12:09:49 -0800

    feat: updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,3 +9,7 @@ alias tree="eza --tree"
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init.fish 2>/dev/null || :
