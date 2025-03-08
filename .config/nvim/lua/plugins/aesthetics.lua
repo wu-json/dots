c0df1093f2971d91710ba4d320eee54b3182aa6d
wu-json <jason.wu2153@gmail.com>
2025-03-08 12:57:30 -0800

    feat: down the lush rabbit hole

--- .config/nvim/lua/plugins/aesthetics.lua
+++ .config/nvim/lua/plugins/aesthetics.lua
@@ -1,5 +1,10 @@
 return {
   {
+    "rktjmp/lush.nvim",
+    -- if you wish to use your own colorscheme:
+    -- { dir = '/absolute/path/to/colorscheme', lazy = true },
+  },
+  {
     "rcarriga/nvim-notify",
     opts = {
       background_colour = "#000000",
