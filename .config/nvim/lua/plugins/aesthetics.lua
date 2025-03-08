2c475eb8c567cce40e7937987a2fd3de0c5cf8fa
wu-json <jason.wu2153@gmail.com>
2025-03-08 12:38:58 -0800

    feat: remove nightfox temp

--- .config/nvim/lua/plugins/aesthetics.lua
+++ .config/nvim/lua/plugins/aesthetics.lua
@@ -1,22 +1,5 @@
 return {
   {
-    "EdenEast/nightfox.nvim",
-    opts = {
-      options = {
-        transparent = true,
-      },
-      groups = {
-        all = {
-          NormalFloat = { fg = "fg1", bg = "NONE" },
-        },
-      },
-    },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = { colorscheme = "nightfox" },
-  },
-  {
     "rcarriga/nvim-notify",
     opts = {
       background_colour = "#000000",
