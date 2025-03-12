3c1f626a2c0f4621640dd60d4b24631e7d146dfe
Jason Wu <jason.wu2153@gmail.com>
2025-03-12 13:29:37 -0700

    feat: updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,3 +12,7 @@ source ~/.orbstack/shell/init.fish 2>/dev/null || :
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
--- .config/nvim/lua/plugins/lang.lua
+++ .config/nvim/lua/plugins/lang.lua
@@ -23,16 +23,6 @@ return {
   {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    config = function()
-      require("typescript-tools").setup({
-        on_attach = function(client)
-          -- Disable formatting from typescript-tools since lazyvim uses conform for
-          -- formatting making this redundant: https://github.com/pmizio/typescript-tools.nvim/issues/288
-          client.server_capabilities.documentFormattingProvider = false
-          client.server_capabilities.documentRangeFormattingProvider = false
-        end,
-      })
-    end,
   },
   {
     "nvim-treesitter/nvim-treesitter",
