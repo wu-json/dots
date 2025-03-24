24cfc582c600e4c4c8b0528f4900777ec149a12d
Jason Wu <jason.wu2153@gmail.com>
2025-03-24 16:10:26 -0700

    feat: 200fps

--- .config/wezterm/visuals.lua
+++ .config/wezterm/visuals.lua
@@ -6,7 +6,7 @@ function module.apply_to_config(config)
 	config.font_size = 16.0
 	config.hide_tab_bar_if_only_one_tab = true
 	config.macos_window_background_blur = 10
-	config.max_fps = 160
+	config.max_fps = 200
 	config.window_background_opacity = 0.95
 	config.window_decorations = "RESIZE"
 end
