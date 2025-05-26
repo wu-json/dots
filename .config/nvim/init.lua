d83c5ffff80b0520f6a32ce42d2473a4b1c456d7
Jason Wu <jason.wu2153@gmail.com>
2025-05-26 02:30:23 -0700

    feat: lang

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Sand stuffles
+vim.filetype.add({
+  extension = { sand = "sand" },
+})
+vim.lsp.config("sand", {
+  name = "sand",
+  cmd = { "sand", "--stdio" },
+  filetypes = { "sand" },
+  root_markers = { ".sand" },
+})
+vim.lsp.enable("sand")
