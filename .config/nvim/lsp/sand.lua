27bb774c83b823f9d0f306dd05c4f539aeae6dee
Jason Wu <jason.wu2153@gmail.com>
2025-05-29 17:54:31 -0700

    feat: lsp

--- .config/nvim/lsp/sand.lua
+++ .config/nvim/lsp/sand.lua
@@ -3,4 +3,25 @@ return {
   cmd = { "sand", "--stdio" },
   filetypes = { "sand" },
   root_markers = { ".sand" },
+  handlers = {
+    -- handle rename request for certain code actions like extracting functions / types
+    ["window/showMessage"] = function(_, result, ctx)
+      local message = result.message or "Unknown message"
+      local message_type = result.type or 1
+
+      if message_type == 1 then -- Error
+        require("snacks").notify.error(message)
+      elseif message_type == 2 then -- Warning
+        require("snacks").notify.warn(message)
+      elseif message_type == 3 then -- Info
+        require("snacks").notify.info(message)
+      elseif message_type == 4 then -- Log
+        require("snacks").notify(message)
+      else
+        require("snacks").notify(message)
+      end
+
+      return vim.NIL
+    end,
+  },
 }
