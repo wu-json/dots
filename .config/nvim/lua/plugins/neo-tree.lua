57791d11c53538e1a8ad5ad1e38f972164900224
Jason Wu <jason.wu2153@gmail.com>
2025-06-19 20:36:51 -0700

    feat: lazy neotree

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,9 +1,6 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  -- NOTE: (wu-json) I would love to lazy load this but there is a bug with this right now
-  -- that causes an empty neo-tree on startup if we don't turn lazy loading off being tracked
-  -- here: https://github.com/nvim-neo-tree/neo-tree.nvim/issues/1699
-  lazy = false,
+  lazy = true,
   opts = {
     filesystem = {
       filtered_items = {
