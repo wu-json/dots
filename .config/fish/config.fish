5dd510ea312beced3fb9dffcf0b6fa07e78f91dc
Jason Wu <jason.wu2153@gmail.com>
2025-06-30 15:53:36 -0700

    feat: assume.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -31,3 +31,7 @@ end
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+# Used for Granted CLI:
+# https://docs.commonfate.io/granted/troubleshooting#manually-configuring-your-shell-profile
+alias assume="source ~/.config/fish/assume.fish"
