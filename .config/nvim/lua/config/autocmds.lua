940d9ab3fa7b8aa9c92af4dabbc9dcea86e191d4
Jason Wu <jason.wu2153@gmail.com>
2025-08-17 11:58:41 -0700

    feat: sand

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Set filetype for sand files
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+  pattern = "*.sand",
+  callback = function()
+    vim.bo.filetype = "sand"
+  end,
+})
