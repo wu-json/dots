decf64302119fbdf3458e1b71a5148c5101fa14a
wu-json <jason.wu2153@gmail.com>
2025-09-28 13:16:01 -0700

    feat(plugins): github-preview

--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -1,6 +1,19 @@
 return {
   { "rhysd/git-messenger.vim" },
   {
+    "wallpants/github-preview.nvim",
+    cmd = { "GithubPreviewToggle" },
+    keys = { "<leader>mpt" },
+    config = function(_, opts)
+      local gpreview = require("github-preview")
+      gpreview.setup(opts)
+      local fns = gpreview.fns
+      vim.keymap.set("n", "<leader>mpt", fns.toggle)
+      vim.keymap.set("n", "<leader>mps", fns.single_file_toggle)
+      vim.keymap.set("n", "<leader>mpd", fns.details_tags_toggle)
+    end,
+  },
+  {
     "yetone/avante.nvim",
     event = "VeryLazy",
     version = "v0.0.25",
