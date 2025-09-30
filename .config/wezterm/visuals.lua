d7539cb7363305c0a6f2ec63d57ab13dbd7f1c8d
Jason Wu <jason.wu2153@gmail.com>
2025-09-29 21:18:10 -0700

    feat: again

--- .config/wezterm/visuals.lua
+++ .config/wezterm/visuals.lua
@@ -7,7 +7,7 @@ function module.apply_to_config(config)
 	config.hide_tab_bar_if_only_one_tab = true
 	config.macos_window_background_blur = 10
 	config.max_fps = 240
-	config.window_background_opacity = 0.95
+	config.window_background_opacity = 0.97
 	config.window_decorations = "RESIZE"
 end
 
