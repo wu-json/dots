d762ea0387cb049ce15c7b818840dc8cfc3fabe6
wu-json <jason.wu2153@gmail.com>
2025-10-12 23:57:40 -0700

    feat: init-fish

--- scripts/init-fish.fish
+++ scripts/init-fish.fish
@@ -10,3 +10,6 @@ end
 # Add aqua to PATH
 fish_add_path (aqua root-dir)/bin
 set -Ux AQUA_GLOBAL_CONFIG (git rev-parse --show-toplevel)/aqua/aqua.yaml
+
+# Unset theme and save (I like the monochrome look)
+fish_config theme save none
