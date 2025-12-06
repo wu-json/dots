d583bfed41cbee9442f21581aee75c44251370e4
wu-json <jason.wu2153@gmail.com>
2025-12-06 10:57:05 -0800

    feat: autosuggestion word by word

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -49,6 +49,13 @@ set fish_greeting ""
 # Turn on vi mode by default
 fish_vi_key_bindings
 
+# Custom key bindings for word-by-word completion
+function fish_user_key_bindings
+    # Cmd-; to accept one word from autosuggestion
+    bind -M insert \e\; forward-word
+    bind -M default \e\; forward-word
+end
+
 # No fish theme because I like monochrome
 yes | fish_config theme save None
 
