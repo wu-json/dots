554e079431dedd0dc8e85a34ab47c49c430a0fac
wu-json <jason.wu2153@gmail.com>
2026-01-21 14:58:42 -0800

    feat: build lsps

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -9,3 +9,23 @@ vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
     vim.bo.filetype = "sand"
   end,
 })
+
+-- Set filetype for Bazel/Buck2 Starlark files
+vim.filetype.add({
+  extension = {
+    bzl = "bzl",
+    bazel = "bzl",
+  },
+  filename = {
+    ["BUILD"] = "bzl",
+    ["BUILD.bazel"] = "bzl",
+    ["WORKSPACE"] = "bzl",
+    ["WORKSPACE.bazel"] = "bzl",
+    ["MODULE.bazel"] = "bzl",
+    ["BUCK"] = "bzl",
+    ["TARGETS"] = "bzl",
+  },
+})
+
+-- Register starlark treesitter parser for bzl filetype
+vim.treesitter.language.register("starlark", "bzl")
