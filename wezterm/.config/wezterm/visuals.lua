45953e100f47d0cd5c0bf49ef1664472c33fc9eb
wu-json <jason.wu2153@gmail.com>
2026-04-16 14:47:53 -0700

    feat: update fonts

--- wezterm/.config/wezterm/visuals.lua
+++ wezterm/.config/wezterm/visuals.lua
@@ -1,8 +1,16 @@
+local wezterm = require("wezterm")
+
 local module = {}
 
 function module.apply_to_config(config)
 	config.color_scheme = "carbonfox"
 	config.enable_tab_bar = false
+	config.font = wezterm.font_with_fallback({
+		"JetBrains Mono",
+		{ family = "Hiragino Sans", assume_emoji_presentation = false },
+		{ family = "Hiragino Mincho ProN", assume_emoji_presentation = false },
+		"Apple Color Emoji",
+	})
 	config.font_size = 15.0
 	config.hide_tab_bar_if_only_one_tab = true
 	config.macos_window_background_blur = 10
