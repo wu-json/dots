a9c11092abef0d13b3e5f3c906045c6f1f5ae869
wu-json <jason.wu2153@gmail.com>
2026-04-16 11:26:52 -0700

    feat: yuki

--- nvim/.config/nvim/lua/plugins/aesthetics.lua
+++ nvim/.config/nvim/lua/plugins/aesthetics.lua
@@ -1,10 +1,13 @@
 return {
+  -- {
+  --   "wu-json/chainsaw.nvim",
+  -- },
   {
-    "wu-json/chainsaw.nvim",
+    "wu-json/yuki.nvim",
   },
   {
     "LazyVim/LazyVim",
-    opts = { colorscheme = "reze" },
+    opts = { colorscheme = "yuki" },
   },
   {
     "j-hui/fidget.nvim",
@@ -32,15 +35,35 @@ return {
         },
         preset = {
           keys = {
-          { icon = " ", key = "f", desc = "ファイルを探す", action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = " ", key = "n", desc = "新規ファイル", action = ":ene | startinsert" },
-          { icon = " ", key = "g", desc = "テキストを探す", action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = " ", key = "r", desc = "最近のファイル", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-          { icon = " ", key = "c", desc = "設定", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-          { icon = " ", key = "s", desc = "セッション復元", section = "session" },
-          { icon = " ", key = "x", desc = "レイジー エクストラ", action = ":LazyExtras" },
-          { icon = "󰒲 ", key = "l", desc = "レイジー", action = ":Lazy" },
-          { icon = " ", key = "q", desc = "終了", action = ":qa" },
+            {
+              icon = " ",
+              key = "f",
+              desc = "ファイルを探す",
+              action = ":lua Snacks.dashboard.pick('files')",
+            },
+            { icon = " ", key = "n", desc = "新規ファイル", action = ":ene | startinsert" },
+            {
+              icon = " ",
+              key = "g",
+              desc = "テキストを探す",
+              action = ":lua Snacks.dashboard.pick('live_grep')",
+            },
+            {
+              icon = " ",
+              key = "r",
+              desc = "最近のファイル",
+              action = ":lua Snacks.dashboard.pick('oldfiles')",
+            },
+            {
+              icon = " ",
+              key = "c",
+              desc = "設定",
+              action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+            },
+            { icon = " ", key = "s", desc = "セッション復元", section = "session" },
+            { icon = " ", key = "x", desc = "レイジー エクストラ", action = ":LazyExtras" },
+            { icon = "󰒲 ", key = "l", desc = "レイジー", action = ":Lazy" },
+            { icon = " ", key = "q", desc = "終了", action = ":qa" },
           },
           header = [[
  ⠀⠀⠀⠀⠀⠀⠀⠀⢰⡀⠀⠀⠀⠀⠀⠀⢠⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -70,6 +93,10 @@ return {
       },
     },
   },
-  -- { "rktjmp/lush.nvim", { dir = "/Users/jasonwu/GitHub/personal/chainsaw.nvim", lazy = true } },
+  {
+    "rktjmp/lush.nvim",
+    -- For testing local themes built with lush
+    -- { dir = "/Users/jasonwu/GitHub/personal/chainsaw.nvim", lazy=true }
+  },
   { "rktjmp/shipwright.nvim" },
 }
