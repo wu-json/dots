18377e753e00e0e3953d767fa02752e80b05004c
wu-json <jason.wu2153@gmail.com>
2026-04-15 18:32:44 -0700

    feat: update dash

--- nvim/.config/nvim/lua/plugins/aesthetics.lua
+++ nvim/.config/nvim/lua/plugins/aesthetics.lua
@@ -31,6 +31,17 @@ return {
           end,
         },
         preset = {
+          keys = {
+          { icon = " ", key = "f", desc = "ファイル検索", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "新規ファイル", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "テキスト検索", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "最近のファイル", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "設定", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "s", desc = "セッション復元", section = "session" },
+          { icon = " ", key = "x", desc = "Lazy 拡張", action = ":LazyExtras" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "終了", action = ":qa" },
+          },
           header = [[
  ⠀⠀⠀⠀⠀⠀⠀⠀⢰⡀⠀⠀⠀⠀⠀⠀⢠⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⢁⠀⠀⠀⠀⠀⠀⣸⢠⠀⠀⠀⠀⠀⠀⠀⠀⠀
