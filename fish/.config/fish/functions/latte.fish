c67783ae9bfc12c970f1485041c3f9687ca442c4
wu-json <jason.wu2153@gmail.com>
2026-04-20 12:28:45 -0700

    feat: caffeinate

---
+++ fish/.config/fish/functions/latte.fish
@@ -0,0 +1,35 @@
+# latte: friendlier `caffeinate` wrapper that takes hours or minutes instead
+# of seconds. Handy for keeping the Mac awake while long-running agents /
+# background jobs do their thing.
+function latte --description 'caffeinate with -h HOURS or -m MINUTES (default 1h)'
+    argparse 'h/hours=' 'm/minutes=' -- $argv
+    or return
+
+    set -l seconds
+    set -l label
+
+    if set -q _flag_hours; and set -q _flag_minutes
+        echo "latte: specify only one of -h/--hours or -m/--minutes" >&2
+        return 1
+    else if set -q _flag_hours
+        set seconds (math -s0 "$_flag_hours * 3600")
+        or begin
+            echo "latte: invalid hours: $_flag_hours" >&2
+            return 1
+        end
+        set label "$_flag_hours hour(s)"
+    else if set -q _flag_minutes
+        set seconds (math -s0 "$_flag_minutes * 60")
+        or begin
+            echo "latte: invalid minutes: $_flag_minutes" >&2
+            return 1
+        end
+        set label "$_flag_minutes minute(s)"
+    else
+        set seconds 3600
+        set label "1 hour"
+    end
+
+    echo "latte: keeping awake for $label ($seconds seconds)"
+    caffeinate -dimsu -t $seconds
+end
