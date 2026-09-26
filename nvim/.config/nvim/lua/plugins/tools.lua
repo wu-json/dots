b0f282d9ed86e65e8c91925d6eda75449383d640
wu-json <jason.wu2153@gmail.com>
2026-09-26 01:02:33 -0700

    Remove markdown-preview.nvim from Neovim config

--- nvim/.config/nvim/lua/plugins/tools.lua
+++ nvim/.config/nvim/lua/plugins/tools.lua
@@ -1,14 +1,3 @@
 return {
   { "rhysd/git-messenger.vim" },
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-    keys = {
-      { "<leader>mm", "<cmd>MarkdownPreviewToggle<cr>", ft = "markdown", desc = "Markdown Preview (browser)" },
-    },
-  },
 }
