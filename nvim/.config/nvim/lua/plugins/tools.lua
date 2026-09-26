8b2bd45783a2145a1eea2f1007d1f3ba49f5db80
wu-json <jason.wu2153@gmail.com>
2026-09-26 00:58:06 -0700

    Remove Kulala from Neovim config

--- nvim/.config/nvim/lua/plugins/tools.lua
+++ nvim/.config/nvim/lua/plugins/tools.lua
@@ -14,21 +14,6 @@ return {
     end,
   },
   {
-    "mistweaverco/kulala.nvim",
-    keys = {
-      { "<leader>Rs", desc = "Send request" },
-      { "<leader>Ra", desc = "Send all requests" },
-      { "<leader>Rb", desc = "Open scratchpad" },
-    },
-    ft = { "http", "rest" },
-    opts = {
-      -- your configuration comes here
-      global_keymaps = true,
-      global_keymaps_prefix = "<leader>R",
-      kulala_keymaps_prefix = "",
-    },
-  },
-  {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
