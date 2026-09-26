769af90ff0e23097dcf525be98d62d86d6fd0710
wu-json <jason.wu2153@gmail.com>
2026-09-26 01:14:42 -0700

    Fix WezTerm border at full background opacity

--- wezterm/.config/wezterm/visuals.lua
+++ wezterm/.config/wezterm/visuals.lua
@@ -15,8 +15,8 @@ function module.apply_to_config(config)
 	config.hide_tab_bar_if_only_one_tab = true
 	config.macos_window_background_blur = 10
 	config.max_fps = 120
-	config.window_background_opacity = 0.99
-	config.window_decorations = "RESIZE"
+	config.window_background_opacity = 1
+	config.window_decorations = "RESIZE | MACOS_FORCE_DISABLE_SHADOW"
 end
 
 return module
