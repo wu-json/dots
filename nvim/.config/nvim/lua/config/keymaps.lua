34730a9824ae8d9d29c20b4eb1b7a6aefa55d86e
wu-json <jason.wu2153@gmail.com>
2026-09-26 01:27:45 -0700

    Add prose mode toggle on leader p

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,25 @@ local map = vim.keymap.set
 map("n", "<leader>E", "<cmd>Explore<cr>", { desc = "Open netrw explorer" })
 map("n", "<leader>bo", "<cmd>BufOnly<cr>", { desc = "Delete all other buffers" })
 map("n", "<leader>yp", "<cmd>let @+ = expand('%:p')<cr>", { desc = "Copy absolute path to clipboard" })
+
+map("n", "<leader>p", function()
+  Snacks.zen({
+    toggles = { dim = false, git_signs = false, mini_diff_signs = false },
+    win = {
+      width = 90,
+      wo = {
+        wrap = true,
+        linebreak = true,
+        breakindent = true,
+        spell = true,
+        number = false,
+        relativenumber = false,
+        signcolumn = "no",
+        foldcolumn = "0",
+        cursorline = false,
+        colorcolumn = "",
+        list = false,
+      },
+    },
+  })
+end, { desc = "Toggle Prose Mode" })
